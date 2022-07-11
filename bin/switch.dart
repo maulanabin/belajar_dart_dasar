3774a8f0974baa335636d644bc8cd89d26d51a40
maulanabin <2041720132@student.polinema.ac.id>
2022-07-11 11:29:44 +0700

    Belajar Dart

---
+++ bin/switch.dart
@@ -0,0 +1,19 @@
+void main() {
+  var nilai = 'E';
+
+  switch(nilai) {
+    case 'A':
+      print('Wow, Anda Lulus dengan Baik');
+      break;
+    case 'B':
+    case 'C':
+      print('Anda Lulus');
+      break;
+    case 'D':
+      print('Anda Tidak Lulus');
+      break;
+    default:
+      print('Mungkin Anda Salah Jurusan');
+
+  }
+}
