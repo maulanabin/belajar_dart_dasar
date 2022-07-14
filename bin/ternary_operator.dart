bbc480ed2a4a050fa5fac25aa4f4712ef842b3f5
maulanabin <2041720132@student.polinema.ac.id>
2022-07-14 08:56:01 +0700

    Belajar Dart

---
+++ bin/ternary_operator.dart
@@ -0,0 +1,12 @@
+void main() {
+  var nilai = 80;
+  var ucapan = nilai >= 75 ? 'Selamat Anda Lulus' : 'Silakan Coba Lagi';
+
+  // if(nilai >= 75) {
+  //   ucapan = 'Selamat Anda Lulus';
+  // } else {
+  //   ucapan = 'Silakan Coba Lagi';
+  // }
+
+  print(ucapan);
+}
